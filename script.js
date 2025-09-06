019337d121084bec7b7f6b92ba9eff376ce5d361
skuz <j.pierre@yandex.ru>
2025-09-06 15:53:46 +0300

    Задача с координатами

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
-console.log('Hello world! Welcome!!!Awesome!');
+/*Написать код, рассчитывающий дистанцию о объекта: 
+
+addressLat - адрес назначения lat (например: 10)
+addressLong - адрес назначения long
+positionLat - текущее положение пользователя lat
+positionLong - текущее положение пользователя long
+В результате на основе этих координат нужно посчитать расстояние 
+от текущего месторасположения до назначения.*/
+
+//Координаты адреса назначения
+const addressLat = 10;
+const addressLong = 20;
+//Координаты текущего положения
+const positionLat = 0;
+const positionLong = 0;
+//
+const Path = Math.sqrt(
+  (addressLat - positionLat) ** 2 + (addressLong - positionLong) ** 2
+);
+console.log(Path);
