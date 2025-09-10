9a95dfed1b19a0ffc9443da3abb273c624102b05
skuz <j.pierre@yandex.ru>
2025-09-10 17:18:53 +0300

    Приветствие в консоли на разых языках

--- script.js
+++ script.js
@@ -1,20 +1,17 @@
-/*Написать код, рассчитывающий дистанцию о объекта: 
+/*В зависимости от языка пользователя, вывести в консоль соответствующие приветствия.*/
+const lang = navigator.language;
 
-addressLat - адрес назначения lat (например: 10)
-addressLong - адрес назначения long
-positionLat - текущее положение пользователя lat
-positionLong - текущее положение пользователя long
-В результате на основе этих координат нужно посчитать расстояние 
-от текущего месторасположения до назначения.*/
+switch (true) {
+  case lang == 'ru':
+    console.log('Привет!');
+    break;
 
-//Координаты адреса назначения
-const addressLat = 10;
-const addressLong = 20;
-//Координаты текущего положения
-const positionLat = 0;
-const positionLong = 0;
-//
-const Path = Math.sqrt(
-  (addressLat - positionLat) ** 2 + (addressLong - positionLong) ** 2
-);
-console.log(Path);
+  case lang == 'en':
+    console.log('Hello!');
+    break;
+
+  case lang == 'de':
+    console.log('Gutten tag!');
+    break;
+}
+//console.log(lang);
