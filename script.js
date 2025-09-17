3f8856d923af3e8eb767c9eb9135459fd4cf4c42
skuz <j.pierre@yandex.ru>
2025-09-17 20:58:39 +0300

    fix1

--- script.js
+++ script.js
@@ -5,20 +5,25 @@
 проверка пароля - принимает зашифрованный пароль и второй пароль. Воспроизводит алгоритм назад 
 на зашифрованном пароле и возвращает true, если он совпадает со втором паролем и false, если нет.
 crypto(‘password’) -> ssapdorw
-
+      password
 check(‘ssapdorw’, ‘password’) -> true
 
 check(‘ssapdorw’, ‘wrong’) -> false*/
-const pass1 = '111';
-const pass2 = '111';
+const pass1 = 'password';
+const pass2 = 'password';
 
 function crypto(pass) {
   const mas = pass.split('').reverse();
   //console.log(mas);
-  const mm = mas.splice(2, 2);
+  const mm = mas.splice(-4, 4);
+  const el1 = mas.slice(1, 2).join('');
+  const el2 = mas.slice(2, 3).join('');
+  mas[1] = el2;
+  mas[2] = el1;
   //console.log(mas);
   //console.log(mm);
-  const cryptoPass = mas.concat(mm).join('');
+  //console.log(mm2);
+  const cryptoPass = mm.concat(mas).join('');
   //console.log(cryptoPass);
   return cryptoPass;
 }
@@ -28,16 +33,18 @@ crypto(pass1);
 function decrypto(pass, checkpass) {
   const oldPass = crypto(pass).split('');
   //console.log(oldPass);
-  const mm = oldPass.splice(-2, 2);
+  const mm = oldPass.splice(-4, 4);
   //console.log(mm);
   //console.log(oldPass);
-  const mm2 = oldPass.splice(0, 2);
-  //console.log(mm2);
-  //console.log(oldPass);
+  const el1 = mm.slice(1, 2).join('');
+  const el2 = mm.slice(2, 3).join('');
+  mm[1] = el2;
+  mm[2] = el1;
+
+  //console.log(mm);
   const deCryptopass =
-    checkpass === mm2.concat(mm).concat(oldPass).reverse().join('')
-      ? true
-      : false;
+    checkpass === mm.concat(oldPass).reverse().join('') ? true : false;
+  //console.log(mm.concat(oldPass).reverse().join(''));
   console.log(deCryptopass);
   return deCryptopass;
 }
