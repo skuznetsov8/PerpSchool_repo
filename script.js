68a1906ffabdb16b2b0720b4bc6a75120f078bfd
skuz <j.pierre@yandex.ru>
2025-09-17 19:05:47 +0300

    default

--- script.js
+++ script.js
@@ -1,74 +1,45 @@
-function valuta(money, currency, newValuta) {
-  let kurs;
-  let newMoney;
-  switch (newValuta) {
-    case 'rub':
-    case 'usd':
-    case 'eur':
-    case 'gbp':
-      if (currency === newValuta) {
-        kurs = 1;
-        console.log(`Курс 1 ${currency} = 1 ${newValuta}`);
-        break;
-      } else {
-        switch (currency) {
-          case 'rub':
-            newValuta == 'usd'
-              ? (kurs = Number(100))
-              : newValuta == 'eur'
-              ? (kurs = Number(120))
-              : newValuta == 'gbp'
-              ? (kurs = Number(150))
-              : (kurs = null);
+/*Написать 2 функции:
 
-            console.log(`Курс 1 ${currency} = ${kurs} ${newValuta}`);
-            break;
-          case 'usd':
-            newValuta == 'rub'
-              ? (kurs = Number(100))
-              : newValuta == 'eur'
-              ? (kurs = Number(1.1))
-              : newValuta == 'gbp'
-              ? (kurs = Number(1.5))
-              : (kurs = null);
-            console.log(`Курс 1 ${currency} = ${kurs} ${newValuta}`);
-            break;
-          case 'eur':
-            newValuta == 'usd'
-              ? (kurs = Number(0.3))
-              : newValuta == 'rub'
-              ? (kurs = Number(200))
-              : newValuta == 'gbp'
-              ? (kurs = Number(0.7))
-              : (kurs = null);
-            console.log(`Курс 1 ${currency} = ${kurs} ${newValuta}`);
-            break;
-          case 'gbp':
-            newValuta == 'usd'
-              ? (kurs = Number(0.5))
-              : newValuta == 'eur'
-              ? (kurs = Number(0.8))
-              : newValuta == 'rub'
-              ? (kurs = Number(300))
-              : (kurs = null);
-            console.log(`Курс 1 ${currency} = ${kurs} ${newValuta}`);
-            break;
-          default:
-            kurs = null;
-            console.log('Такую валюту не умеем');
-        }
-      }
-      newMoney = money * kurs;
-      console.log(newMoney);
-      return newMoney;
-      break;
+шифратор пароля - функция принимает пароль, разбивает по символам, меняет местами 
+какие-то буквы по заданному алгоритму и возвращает строку.
+проверка пароля - принимает зашифрованный пароль и второй пароль. Воспроизводит алгоритм назад 
+на зашифрованном пароле и возвращает true, если он совпадает со втором паролем и false, если нет.
+crypto(‘password’) -> ssapdorw
 
-    default:
-      console.log('Такую валюту не умеем');
-      newMoney = null;
-      return newMoney;
-      break;
-  }
+check(‘ssapdorw’, ‘password’) -> true
+
+check(‘ssapdorw’, ‘wrong’) -> false*/
+const pass1 = '111';
+const pass2 = '111';
+
+function crypto(pass) {
+  const mas = pass.split('').reverse();
+  //console.log(mas);
+  const mm = mas.splice(2, 2);
+  //console.log(mas);
+  //console.log(mm);
+  const cryptoPass = mas.concat(mm).join('');
+  //console.log(cryptoPass);
+  return cryptoPass;
 }
 
-valuta(150, 'gbp', 'eur');
+crypto(pass1);
+
+function decrypto(pass, checkpass) {
+  const oldPass = crypto(pass).split('');
+  //console.log(oldPass);
+  const mm = oldPass.splice(-2, 2);
+  //console.log(mm);
+  //console.log(oldPass);
+  const mm2 = oldPass.splice(0, 2);
+  //console.log(mm2);
+  //console.log(oldPass);
+  const deCryptopass =
+    checkpass === mm2.concat(mm).concat(oldPass).reverse().join('')
+      ? true
+      : false;
+  console.log(deCryptopass);
+  return deCryptopass;
+}
+
+decrypto(pass1, pass2);
