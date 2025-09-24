6c1a7ca87c2c72eee34e6266d25c0b18d544391f
skuz <j.pierre@yandex.ru>
2025-09-24 12:36:47 +0300

    fix-0

--- script.js
+++ script.js
@@ -1,33 +1,36 @@
-function sortWithLoops(arr) {
-  // создаем копию массива, чтобы не мутировать исходный
-  const newArray = arr.slice();
+/*Напишите функцию, которая принимает:
+Массив чисел
+Функцию удаления элементов
+И возвращает отфильтрованный массив. При это функция удаления 
+элементов принимает число и возвращает true, если его надо 
+удалить и false, если надо оставить.
 
-  // внешний цикл по позициям от начала до предпоследней
-  for (let i = 0; i < newArray.length - 1; i++) {
-    // предполагаем наименьший элемент текущей подстановки
-    console.log(`i = ${i}`);
-    let minIndex = i;
+Для функции удаления, удаляющей все числа больше 5:
 
-    // внутренний цикл ищет минимальный элемент в оставшейся части
-    for (let j = i + 1; j < newArray.length; j++) {
-      console.log(`- j = ${j}`);
-      if (newArray[j] < newArray[minIndex]) {
-        console.log(`!!! ${newArray[j]} < ${newArray[minIndex]}`);
-        minIndex = j;
-      }
-    }
+Входной массив: [3, 6, 9, 2]
+Выходной массив: [3, 2] (числа 6 и 9 удалены, так как они больше 5)
+*/
 
-    // обмен, если найден элемент меньше текущего
-    if (minIndex !== i) {
-      const tmp = newArray[i];
-      newArray[i] = newArray[minIndex];
-      newArray[minIndex] = tmp;
-    }
-  }
-
-  return newArray;
+function delInArr(x) {
+  //console.log(`111 x  --  ${x}`);
+  //console.log(`111  --  ${x > 5}`);
+  return x > 5;
 }
 
-// Пример:
-const arr = [1, 40, -5, 10, 0];
-console.log(sortWithLoops(arr)); // [-5, 0, 1, 10, 40]
+function filterArr(arr, fn) {
+  for (let i = 0; i < arr.length; ) {
+    //console.log(`2 --- [i] = ${i}, arr[i] -  ${arr[i]}`);
+    let res = fn(arr[i]);
+    //console.log(`22 - res -  ${res}`);
+    if (res) {
+      // console.log(`222 - fn(arr[i]) -  ${res}`);
+      arr.splice(i, 1);
+    } else {
+      i++;
+    }
+  }
+  console.log(arr);
+  return arr;
+}
+
+filterArr([3, 6, 9, 2], delInArr);
